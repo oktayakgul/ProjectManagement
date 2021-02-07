7a79de3f5e6ec289b75121155c6f8a743b8319f1
Oktay Akgul <oktay.akgul@turkcell.com.tr>
2021-02-07 17:01:30 +0300

    init

---
+++ src/test/java/com/oa/pma/ProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oa.pma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
