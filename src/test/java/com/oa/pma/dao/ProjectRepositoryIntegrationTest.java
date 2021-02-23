bdac26649a4b2df1ad0ed56b370f8e2d315d875c
Oktay Akgul <oktay.akgul@turkcell.com.tr>
2021-02-24 02:11:35 +0300

    - Using @SpringBootTest Annotation Correctly (don't run sql(s))

--- src/test/java/com/oa/pma/dao/ProjectRepositoryIntegrationTest.java
+++ src/test/java/com/oa/pma/dao/ProjectRepositoryIntegrationTest.java
@@ -1,24 +1,18 @@
-package dao;
+package com.oa.pma.dao;
 
-import com.oa.pma.ProjectManagementApplication;
-import com.oa.pma.dao.ProjectRepository;
 import com.oa.pma.entity.Project;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
-@ContextConfiguration(classes = ProjectManagementApplication.class)
+@SpringBootTest
 @RunWith(SpringRunner.class)
-@DataJpaTest
-@SqlGroup({@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = {"classpath:schema.sql","classpath:data.sql"}),
-		@Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:drop.sql")} /* actually, no really need drop.sql */)
 public class ProjectRepositoryIntegrationTest { //en yakin  application.properties dosyasini kullanir
 	
 	@Autowired
@@ -29,7 +23,7 @@ public class ProjectRepositoryIntegrationTest { //en yakin  application.properti
 		Project project = new Project("Test 1","COMPLETE","Descripton for test");
 		projectRepository.save(project);
 		
-		assertEquals(5, projectRepository.findAll().size());
+		assertEquals(1, projectRepository.findAll().size());
 		
 	}
 }
