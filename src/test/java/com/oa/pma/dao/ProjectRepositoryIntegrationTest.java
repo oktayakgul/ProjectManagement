ac602b1c839a63907a2ccc05a399f645c16a6317
Oktay Akgul <oktay.akgul@turkcell.com.tr>
2021-02-24 02:12:13 +0300

    - Using @SpringBootTest Annotation Correctly

--- src/test/java/com/oa/pma/dao/ProjectRepositoryIntegrationTest.java
+++ src/test/java/com/oa/pma/dao/ProjectRepositoryIntegrationTest.java
@@ -13,6 +13,8 @@ import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@SqlGroup({@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = {"classpath:schema.sql","classpath:data.sql"}),
+		@Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:drop.sql")} /* actually, no really need drop.sql */)
 public class ProjectRepositoryIntegrationTest { //en yakin  application.properties dosyasini kullanir
 	
 	@Autowired
@@ -23,7 +25,7 @@ public class ProjectRepositoryIntegrationTest { //en yakin  application.properti
 		Project project = new Project("Test 1","COMPLETE","Descripton for test");
 		projectRepository.save(project);
 		
-		assertEquals(1, projectRepository.findAll().size());
+		assertEquals(5, projectRepository.findAll().size());
 		
 	}
 }
