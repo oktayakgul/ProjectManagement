19a95cb90bbbd9b3d38ecf0f894269feef829b0b
Oktay Akgul <oktay.akgul@turkcell.com.tr>
2021-03-05 02:29:39 +0300

    Spring AOP P.1 : Pointcut and Advice (Before, After)

--- src/main/java/com/oa/pma/logging/ApplicationLoggerAspect.java
+++ src/main/java/com/oa/pma/logging/ApplicationLoggerAspect.java
@@ -1,11 +1,15 @@
 package com.oa.pma.logging;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
@@ -13,14 +17,17 @@ public class ApplicationLoggerAspect {
 	
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	
-	@Pointcut("within(com.oa.pma.controller..*)"
-	+ " || within(com.oa.pma.dao..*) ")
+	@Pointcut("within(com.oa.pma.controller..*)")
 	public void definePackagePointcuts(){
 		// empty method just to name the location specified in the pointcut
 	}
 	
-	@After("definePackagePointcuts()") // or @Before
-	public void log(){
-		log.debug("-----------------------------------------------------------------------------");
+	@After("definePackagePointcuts()") // at Before you dont have any info, args
+	public void logBefore(JoinPoint jp){
+		log.debug("\n-----------------------------After------------------------------\n {}.{} with arguments [s] = {}",
+				jp.getSignature().getDeclaringTypeName(),
+				jp.getSignature().getName(),
+				Arrays.toString(jp.getArgs()));
+		log.debug("\n----------------------------------------------------------------\n");
 	}
 }
