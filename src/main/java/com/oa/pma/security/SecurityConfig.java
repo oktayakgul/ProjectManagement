969e0bb22a91717b4be619bcb587c2cba07b11a7
Oktay Akgul <oktay.akgul@turkcell.com.tr>
2021-05-17 10:59:56 +0300

    Section 12: Spring Security : JDBC Authentication

--- src/main/java/com/oa/pma/security/SecurityConfig.java
+++ src/main/java/com/oa/pma/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.oa.pma.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,13 +10,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	DataSource dataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
+		auth.jdbcAuthentication().dataSource(dataSource)
+		    .withDefaultSchema()
 		        .withUser("admin").password("pass").roles("MASTER")
 		    .and()
 		        .withUser("user1").password("pass1").roles("SLAVE")
@@ -35,6 +42,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		    .antMatchers("/employee/new").hasRole("MASTER")
 			.antMatchers("/").authenticated()
 		.and().formLogin();
+		
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
 		
 	}
 }
